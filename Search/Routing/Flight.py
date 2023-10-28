185f2841db0bc07b1332c1f6c05457a69ab5b7db
FatemeDallal <dallalfateme2512003@gmail.com>
2023-10-28 21:17:24 +0330

    change 1 create graph / after start

--- Search/Routing/Flight.py
+++ Search/Routing/Flight.py
@@ -2,9 +2,9 @@
 class Flight:
     def __init__(self,
                  airline, source_airport, destination_airport, source_airport_city, source_airport_country,
-                 source_airport_latitude, source_airport_longitude, destination_airport_city,
+                 source_airport_latitude, source_airport_longitude, source_airport_altitude, destination_airport_city,
                  destination_airport_country, destination_airport_latitude, destination_airport_longitude,
-                 distance, fly_time, price):
+                 destination_airport_altitude, distance, fly_time, price):
         self.airline = airline
         self.source_airport = source_airport
         self.destination_airport = destination_airport
@@ -12,10 +12,35 @@ class Flight:
         self.source_airport_country = source_airport_country
         self.source_airport_latitude = source_airport_latitude
         self.source_airport_longitude = source_airport_longitude
+        self.source_airport_altitude = source_airport_altitude
         self.destination_airport_city = destination_airport_city
         self.destination_airport_country = destination_airport_country
         self.destination_airport_latitude = destination_airport_latitude
         self.destination_airport_longitude = destination_airport_longitude
+        self.destination_airport_altitude = destination_airport_altitude
         self.distance = distance
         self.fly_time = fly_time
         self.price = price
+
+    def __eq__(self, other):
+        return (self.airline == other.airline and self.source_airport == other.source_airport and
+                self.destination_airport == other.destination_airport and
+                self.source_airport_city == other.source_airport_city and
+                self.source_airport_country == other.source_airport_country and
+                self.source_airport_latitude == other.source_airport_latitude and
+                self.source_airport_longitude == other.source_airport_longitude and
+                self.source_airport_altitude == other.source_airport_altitude and
+                self.destination_airport_city == other.destination_airport_city and
+                self.destination_airport_country == other.destination_airport_country and
+                self.destination_airport_latitude == other.destination_airport_latitude and
+                self.destination_airport_longitude == other.destination_airport_longitude and
+                self.destination_airport_altitude == other.destination_airport_altitude and
+                self.distance == other.distance and self.fly_time == other.fly_time and self.price == other.price)
+
+    def __hash__(self):
+        return hash((self.airline, self.source_airport, self.destination_airport,
+                     self.source_airport_city, self.source_airport_country, self.source_airport_latitude,
+                     self.source_airport_longitude, self.source_airport_altitude, self.destination_airport_city,
+                     self.destination_airport_country, self.destination_airport_latitude,
+                     self.destination_airport_longitude, self.destination_airport_altitude,
+                     self.distance, self.fly_time, self.price))
--- Search/Routing/main.py
+++ Search/Routing/main.py
@@ -2,24 +2,30 @@ import networkx as nx
 import pandas as pd
 
 from Search.Routing.Airport import Airport
+from Search.Routing.Flight import Flight
 
 if __name__ == '__main__':
 
     # read dateset
     dataframe = pd.read_csv('Flight_Data.csv')
 
-    airports = set()
+    # create graph
+    graph = nx.Graph()
 
     for index, row in dataframe.iterrows():
-        airports.add(Airport(row['SourceAirport'], row['SourceAirport_City'], row['SourceAirport_Country']))
-        airports.add(Airport(row['DestinationAirport'], row['DestinationAirport_City'], row['DestinationAirport_Country']))
+        # create node
+        source = Airport(row['SourceAirport'], row['SourceAirport_City'], row['SourceAirport_Country'])
+        destination = Airport(row['DestinationAirport'], row['DestinationAirport_City'], row['DestinationAirport_Country'])
+        graph.add_nodes_from([source, destination])
 
-    graph = nx.Graph
+        # create edge
+        flight = Flight(row['Airline'], row['SourceAirport'], row['DestinationAirport'], row['SourceAirport_City'],
+                        row['SourceAirport_Country'], row['SourceAirport_Latitude'], row['SourceAirport_Longitude'],
+                        row['SourceAirport_Altitude'], row['DestinationAirport_City'], row['DestinationAirport_Country'],
+                        row['DestinationAirport_Latitude'], row['DestinationAirport_Longitude'],
+                        row['DestinationAirport_Altitude'], row['Distance'], row['FlyTime'], row['Price'])
 
-    for airport in airports:
-        graph.add_node(airport)
+        graph.add_weighted_edges_from([(source, destination, flight)])
 
-    flights = list()
-    for index, row in dataframe.iterrows():
-        pass
+    print(graph)
 
