b8b4a2eb62bf02e5e038de61227dd08492b7446a
Zahra Baghi <zahrabaghim127@gmail.com>
2023-11-11 00:37:39 +0330

    add evaluation

--- Search/Regression/regression.py
+++ Search/Regression/regression.py
@@ -2,10 +2,15 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+
+
 
 
 class LinearRegression:
-    def __init__(self, learning_rate=1e-3, n_iters=1000):
+    def __init__(self, learning_rate, n_iters=1000):
         # init parameters
         self.lr = learning_rate
         self.n_iters = n_iters
@@ -106,8 +111,11 @@ y = np.array(df['price'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1,shuffle=True)
 
-linreg = LinearRegression(learning_rate=0.1, n_iters=1000)
+linreg = LinearRegression(learning_rate=0.0001, n_iters=1000)
 linreg.fit(X_train, y_train)
 
 predictions = linreg.predict(X_test)
 print(f"RMSE: {rmse(y_test, predictions)}")
+print(mean_squared_error(y_test,predictions))
+print(mean_absolute_error(y_test,predictions))
+print(r2_score(y_test,predictions))
